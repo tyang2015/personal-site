86650e7c99e0909b5d26554c922bb83b9823a369
tiffany yang <tiffanyang2015@gmail.com>
2022-10-17 00:28:45 -0700

    pushing personal site test1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <div className="portfolio-site-main-container">
+        <div className="introduction-left-pane">
+          <h3> Hi, I'm Tiffany Yang, a software engineer with a passion for Taiwanese food, gaming, and art</h3>
+          
+
+        </div>
+        <div className="introduction-right-pane">
+
+        </div>
+
+      </div>
+    </>
   );
 }
 
