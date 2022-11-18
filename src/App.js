de0cd9885ae381613a566ccbb2f73d54254c52db
tiffany yang <tiffanyang2015@gmail.com>
2022-11-18 00:45:07 -0800

    restyled whole website

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ function App() {
       <div className="portfolio-site-main-container">
         <div className="introduction-left-pane">
           <h3> Hi, I'm Tiffany Yang, a software engineer with a passion for Taiwanese food, gaming, and art</h3>
-          
-
         </div>
         <div className="introduction-right-pane">
 
